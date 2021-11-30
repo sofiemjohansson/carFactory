155fbe215509643bcffcaf262d060fdefab0ab92
sofiemjohansson <sofiem.eriksson@gmail,com>
2021-11-30 12:45:29 +0100

    update

---
+++ src/main/java/main/java/Car.java
@@ -0,0 +1,8 @@
+package main.java;
+
+public interface Car{
+    public String getBrand();
+    public String getColor();
+    public String getRegistrationNumber();
+    public String getEngine();
+}
